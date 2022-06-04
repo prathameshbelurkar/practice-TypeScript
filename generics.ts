06316c6b47e90be54feee19546c6650b57ab4b6f
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-04 16:59:29 +0530

    typescript: generics

---
+++ generics.ts
@@ -0,0 +1,31 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnything<string>(["a", "b", "c"]);
+
+// Example of generics with functions
+
+function printStrings() {}
+
+function printNumbers() {}
